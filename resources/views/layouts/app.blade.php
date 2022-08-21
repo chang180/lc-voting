06d6514297ec2a6a14ca8660bd08df805fb9682e
CHANG180-PC\chang <chang180@gmail.com>
2022-08-21 15:22:04 +0800

    ep4 completed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,25 +51,73 @@
     </header>
     <main class="container mx-auto max-w-custom flex">
         <div class="w-70 mr-5">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum accusantium ipsam molestiae sunt! Minus fuga
-            aliquam nostrum maiores est ex debitis, pariatur iste dolore odit. Et inventore omnis magni! Incidunt
-            officiis fugiat reprehenderit iste quos totam culpa, neque labore sequi animi? Neque voluptates
-            exercitationem eligendi non, hic ipsam voluptatem dolor ex recusandae adipisci tempore, quas assumenda
-            temporibus itaque eum distinctio explicabo ipsa quidem molestiae. Commodi labore exercitationem porro
-            veritatis, aliquam, a dolorem aspernatur corporis dolore maxime nihil amet ad vero dolor ut magni
-            necessitatibus doloribus nulla ipsa assumenda praesentium animi deleniti sit? Labore esse omnis ad maiores
-            alias distinctio sapiente!
+            <div class="bg-white border-2 border-blue rounded-xl mt-16"
+                style="
+                border-image-source: linear-gradient(to bottom, rgba(50,138,241,0.22),rgba(99,123,255,0));
+                border-image-slice:1;
+                background-image: linear-gradient(to bottom, #ffffff,#ffffff),linear-gradient(to bottom, rgba(50,138,241,0.22),rgba(99,123,255,0)));
+                background-origin:border-box;
+                background-clip:content-box,border-box;
+                "
+            >
+                <div class="text-center px-6 py-2 pt-6">
+                    <h3 class="font-semibold text-base">Add an idea</h3>
+                    <p class="text-xs mt-4">Let us know what you would lik and we'll take a look up!!</p>
+                </div>
+                <form action="#" method="post" class="space-y-4 px-4 py-6">
+                    <div>
+                        <input type="text" name="" id=""
+                            class="w-full text-sm border-none bg-gray-100 rounded-xl placeholder-gray-900 px-4 py-2"
+                            placeholder="Your idea">
+                    </div>
+                    <div>
+                        <select name="category_add" id="category_add"
+                            class="w-full bg-gray-100 text-sm rounded-xl border-none px-4 py-2">
+                            <option value="Category One">Category One</option>
+                            <option value="Category Two">Category Two</option>
+                            <option value="Category Three">Category Three</option>
+                            <option value="Category Four">Category Four</option>
+                        </select>
+                    </div>
+                    <div>
+                        <textarea name="idea" id="idea" cols="30" rows="4"
+                            class="w-full bg-gray-100 text-sm rounded-xl placeholder-gray-900 border-none px-4 py-2"
+                            placeholder="Describe your idea"></textarea>
+                    </div>
+                    <div class="flex items-center jstify-between space-x-3">
+                        <button type="button"
+                            class="flex items-center justify-center w-1/2 h-11 text-xs bg-gray-200 font-semibold rounded-xl border border-gray-200 hover:border-gray-400 transition duration-150 ease-in px-6 py-3">
+                            <svg class="h-4 w-4 text-gray-600 transform -rotate-45" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
+                            </svg>
+                            <span class="ml-1">Attach</span>
+                        </button>
+                        <button type="submit"
+                            class="flex items-center justify-center w-1/2 h-11 text-xs bg-blue text-white font-semibold rounded-xl border border-blue hover:bg-blue-hover transition duration-150 ease-in px-6 py-3">
+                            <span class="ml-1">Submit</span>
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
         <div class="w-175">
             <nav class="flex items-center justify-between text-xs">
                 <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
                     <li><a href="#" class="border-b-4 pb-3 border-blue">All ideas(87)</a></li>
-                    <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Considering(6)</a></li>
-                    <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">In Progress(1)</a></li>
+                    <li><a href="#"
+                            class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Considering(6)</a>
+                    </li>
+                    <li><a href="#"
+                            class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">In
+                            Progress(1)</a></li>
                 </ul>
                 <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
                     <li><a href="#" class="border-b-4 pb-3 border-blue">Implemented(87)</a></li>
-                    <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Closed(6)</a></li>
+                    <li><a href="#"
+                            class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Closed(6)</a>
+                    </li>
                 </ul>
             </nav>
 
