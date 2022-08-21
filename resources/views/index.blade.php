c181f0e898f317d142ae4f96a57b4216214a45dc
CHANG180-PC\chang <chang180@gmail.com>
2022-08-21 13:02:11 +0800

    ep3 completed

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+    <!-- filters -->
     <div class="filters flex space-x-6">
         <div class="w-1/3">
             <select name="category" id="category" class="w-full rounded-xl border-none px-4 py-2">
@@ -10,7 +11,7 @@
         </div>
         <div class="w-1/3">
             <select name="other_filters" id="other_filters" class="w-full rounded-xl border-none px-4 py-2">
-                <option value="Filter One">Filter One</option>
+                <option value="Filter One">Filter One</option>`
                 <option value="Filter Two">Filter Two</option>
                 <option value="Filter Three">Filter Three</option>
                 <option value="Filter Four">Filter Four</option>
@@ -18,7 +19,8 @@
         </div>
         <div class="w-2/3 relative">
             <input type="search" placeholder="Find an idea"
-                class="w-full rounded-xl bg-white border-none placeholder-gray-900 px-4 py-2 pl-8" name="" id="">
+                class="w-full rounded-xl bg-white border-none placeholder-gray-900 px-4 py-2 pl-8" name=""
+                id="">
             <div class="absolute top-0 flex items-center h-full ml-2">
                 <svg class="w-4 text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor"
                     stroke-width="2">
@@ -27,5 +29,74 @@
                 </svg>
             </div>
         </div>
-    </div> <!-- end filters -->
+    </div>
+    <!-- end filters -->
+    <div class="ideas-container space-y-6 my-6">
+        <div class="idea-container hover:shadow-card transition duration-150 ease-in bg-white rounded-xl flex cursor-pointer">
+            <div class="border-r border-gray-100 px-5 py-8">
+                <div class="text-center">
+                    <div class="font-semibold text-2xl">12</div>
+                    <div class="text-gray-500">Votes</div>
+                </div>
+
+                <div class="mt-8">
+                    <button
+                        class="w-20 bg-gray-200 border border-gray-200 hover:border-gray-400 font-bold text-xxs uppercase rounded-xl transition duration-150 ease-in px-4 py-3">Vote</button>
+                </div>
+            </div>
+            <div class="flex px-2 py-6">
+                <a href="#" class="flex-none">
+                    <img src="https://source.unsplash.com/200x200/?face&corp=face&v=1" alt="avatar"
+                        class="w-14 h-14 rounded-xl">
+                </a>
+                <div class="mx-4">
+                    <h4 class="text-xl font-semibold">
+                        <a href="#" class="hover:underline">A random title</a>
+                    </h4>
+                    <div class="text-gray-600 mt-3 line-clamp-3">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus in quae eligendi itaque corrupti
+                        omnis ex illo dolorum fuga ducimus asperiores, facere aspernatur. Ipsum, quo. Nobis a excepturi
+                        corporis, qui consequatur autem animi. Neque, labore rem ad earum repudiandae cupiditate
+                        incidunt expedita assumenda nulla itaque hic, maiores, harum officia eum! Voluptates dolor
+                        tempora quae nisi impedit repellendus nam architecto voluptatibus, itaque laborum expedita,
+                        laboriosam ipsum, beatae inventore officiis adipisci minus suscipit ex? Tempora inventore
+                        voluptas vitae sapiente expedita, eligendi earum suscipit quas perspiciatis quibusdam veritatis
+                        vero nulla debitis fugiat ratione facilis fuga minima porro impedit est aliquid quis? Ullam,
+                        nobis.
+                    </div>
+                    <div class="flex items-center justify-between mt-6">
+                        <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
+                            <div>10 hours ago</div>
+                            <div>&bull;</div>
+                            <div>Category 1</div>
+                            <div>&bull;</div>
+                            <div class="text-gray-900">3 Comments</div>
+                        </div>
+                        <div class="flex items-center space-x-2">
+                            <div
+                                class="bg-gray-200 text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 px-4 py-2">
+                                Open
+                            </div>
+                            <button
+                                class="relative bg-gray-100 hover:bg-gray-200 rounded-full h-7 transition duration-150 ease-in py-2 px-3">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                    fill="currentColor" class="bi bi-three-dots" viewBox="0 0 24 24">
+                                    <path style="color:rgba(163,163,163,.5)"
+                                        d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
+                                </svg>
+                                <ul class="absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl py-3 ml-8">
+                                    <li><a href="#"
+                                            class="hover:bg-gray-100 px-5 py-3 block transition duration-150 ease-in">Mark
+                                            as spam</a></li>
+                                    <li><a href="#"
+                                            class="hover:bg-gray-100 px-5 py-3 block transition duration-150 ease-in">Delete
+                                            post</a></li>
+                                </ul>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div><!-- end idea-container -->
+        </div><!-- end ideas-container -->
+
 </x-app-layout>
