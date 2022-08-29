29e7525b8b1d8691ccda6a6bef7fbb7f242c8d00
chang180 <chang180@gmail.com>
2022-08-29 16:01:49 +0800

    ep23 completed

--- tests/Feature/VoteIndexPageTest.php
+++ tests/Feature/VoteIndexPageTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Feature;
 
-use App\Http\Livewire\IdeaIndex;
 use Tests\TestCase;
 use App\Models\Idea;
 use App\Models\User;
@@ -10,6 +9,8 @@ use App\Models\Vote;
 use App\Models\Status;
 use Livewire\Livewire;
 use App\Models\Category;
+use App\Http\Livewire\IdeaIndex;
+use App\Http\Livewire\IdeasIndex;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -42,7 +43,7 @@ class VoteIndexPageTest extends TestCase
     }
 
     /** @test */
-    public function index_page_correctly_receive_votes_count()
+    public function ideas_index_livewire_component_correctly_receive_votes_count()
     {
         $user = User::factory()->create();
         $userB = User::factory()->create();
@@ -74,10 +75,11 @@ class VoteIndexPageTest extends TestCase
             'idea_id' => $idea->id,
         ]);
 
-        $this->get(route('idea.index'))
+        Livewire::test(IdeasIndex::class)
             ->assertViewHas('ideas', function ($ideas) {
                 return $ideas->first()->votes_count == 2;
             });
+
     }
 
     /** @test */
@@ -139,14 +141,12 @@ class VoteIndexPageTest extends TestCase
             'idea_id' => $idea->id,
         ]);
 
-        $response = $this->actingAs($user)
-            ->get(route('idea.index'));
-
-        $ideaWithVotes = $response['ideas']->items()[0];
+        $idea->votes_count = 1;
+        $idea->voted_by_user = 1;
 
         Livewire::actingAs($user)
             ->test(IdeaIndex::class, [
-                'idea' => $ideaWithVotes,
+                'idea' => $idea,
                 'votesCount' => 5,
             ])
             ->assertSet('hasVoted', true)
@@ -264,14 +264,12 @@ class VoteIndexPageTest extends TestCase
             'idea_id' => $idea->id,
         ]);
 
-        $response = $this->actingAs($user)
-            ->get(route('idea.index'));
-
-        $ideaWithVotes = $response['ideas']->items()[0];
+        $idea->votes_count = 1;
+        $idea->voted_by_user = 1;
 
         Livewire::actingAs($user)
             ->test(IdeaIndex::class, [
-            'idea' => $ideaWithVotes,
+            'idea' => $idea,
             'votesCount' => 5,
         ])
             ->call('vote')
