f5969fe0ded534e8eb11d662722bb00ad27c2068
chang180 <chang180@gmail.com>
2022-08-30 13:30:38 +0800

    ep27 completed

--- app/Http/Livewire/StatusFilters.php
+++ app/Http/Livewire/StatusFilters.php
@@ -9,32 +9,29 @@ use Illuminate\Support\Facades\Route;
 
 class StatusFilters extends Component
 {
-    public $status = 'All';
+    public $status;
     public $statusCount;
-
-    protected $queryString = [
-        'status',
-    ];
 
     public function mount()
     {
         $this->statusCount = Status::getCount();
+        $this->status = request()->status ?? 'All';
 
         if (Route::currentRouteName() == 'idea.show') {
             $this->status = null;
-            $this->queryString = [];
         }
     }
 
     public function setStatus($newStatus)
     {
         $this->status = $newStatus;
+        $this->emit('queryStringUpdatedStatus', $this->status);
 
-        // if ($this->getPreviousRouteName() === 'idea.show') {
+        if ($this->getPreviousRouteName() === 'idea.show') {
             return redirect()->route('idea.index', [
                 'status' => $this->status,
             ]);
-        // }
+        }
     }
 
     public function render()
@@ -46,5 +43,4 @@ class StatusFilters extends Component
     {
         return app('router')->getRoutes()->match(app('request')->create(url()->previous()))->getName();
     }
-
 }
