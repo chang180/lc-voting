db39ff6dde0c797dbfc9ccf3d130c95af20cb4c4
chang180 <chang180@gmail.com>
2022-09-02 13:37:54 +0800

    pcntl not available for windows

--- app/Http/Livewire/SetStatus.php
+++ app/Http/Livewire/SetStatus.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Livewire;
 
+use App\Jobs\NotifyAllVoters;
 use App\Models\Idea;
 use App\Models\Status;
 use Livewire\Component;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Mail;
-use App\Mail\IdeaStatusUpdatedMailable;
 
 class SetStatus extends Component
 {
@@ -33,24 +33,10 @@ class SetStatus extends Component
         ]);
 
         if ($this->notifyAllVoters) {
-            $this->notifyAllVoters();
+            NotifyAllVoters::dispatch($this->idea);
         }
 
         $this->emit('statusWasUpdated');
-    }
-
-    public function notifyAllVoters()
-    {
-        $voters = $this->idea->votes()
-            ->select('name', 'email')
-            ->chunk(100, function ($voters) {
-                foreach($voters as $user) {
-                    Mail::to($user)
-                        ->queue(new IdeaStatusUpdatedMailable($this->idea));
-                }
-            });
-
-        
     }
 
     public function render()
